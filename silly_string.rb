e7bb2dd743f086c3249df81743517cda1fc0006e
Ashley Minor <aminor@kipphouston.org>
2018-07-09 22:17:02 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,6 +1,6 @@
 your_favorite_word = "Aspen"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is" + your_favorite_word
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
